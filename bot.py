8db457b1c30660129e421191753a21986d4d8d32
Minong <minhk3259@gmail.com>
2019-05-08 11:17:07 +0900

    deploy_test

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import telegram
 dir(telegram)
 
+
 my_token = '695803293:AAG5FyA1ptFScX6E1ZGtOL4n2aj7CiuLkZM'
-bot = telegram.Bot(token = my_token)
+print('init bot')
+bot = telegram.Bot(token=my_token)
 updates = bot.getUpdates()
 
 result = bot.sendMessage(chat_id='-1001166284442', text="I'm bot")
