db319bcf3bc1b66f587dce1458cd83d1150dc15d
Minong <minhk3259@gmail.com>
2019-05-09 17:52:24 +0900

    clear_bot_error

--- bot.py
+++ bot.py
@@ -7,29 +7,36 @@ dir(telegram)
 bot_token = os.getenv('BOT_TOKEN')
 
 sched = BlockingScheduler()
-if bot_token == None:
-    bot_token = '695803293:AAG5FyA1ptFScX6E1ZGtOL4n2aj7CiuLkZM'
 bot = telegram.Bot(token=bot_token)
+current = []
 
 
-@sched.scheduled_job('interval', minutes=25)
-def timed_job():
-    updates = bot.getUpdates()
-
-
-@sched.scheduled_job('cron', hour=8)
-def scheduled_job():
+def sendMessage():
     message = ''
     platforms = getPlatforms()
 
     for platform in platforms:
         if len(platform['time']) > 9:
+            curent.append(platform)
             continue
         message = platform['title']+'\n'+platform['link']+'\n\n'
         links = getAppLinks(platform['link'])
         for link in links:
             message += link + '\n'
         result = bot.sendMessage(chat_id='-1001166284442', text=message)
+
+
+@sched.scheduled_job('interval', minutes=25)
+def timed_job():
+    if (not current):
+        sendMessage()
+    updates = bot.getUpdates()
+
+
+@sched.scheduled_job('cron', hour=8)
+def scheduled_job():
+    current = []
+    sendMessage()
 
 
 sched.start()
